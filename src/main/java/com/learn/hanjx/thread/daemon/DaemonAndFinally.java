52070ff93637fffb9fe53b0617f7082ff10ca064
511572653@qq.com <511572653@qq.com>
2017-11-23 23:04:04 +0800

    守护线程完善

--- src/main/java/com/learn/hanjx/thread/daemon/DaemonAndFinally.java
+++ src/main/java/com/learn/hanjx/thread/daemon/DaemonAndFinally.java
@@ -6,16 +6,21 @@ public class DaemonAndFinally implements Runnable {
     public static void main(String[] args) {  
         System.out.println("Daemon Thread test..");  
         Thread t = new Thread(new DaemonAndFinally());  
-        t.setDaemon(true);  
+        t.setDaemon(true);
+        t.setName("守护线程");
         Thread tuser = new Thread(new DaemonAndFinally());
+        tuser.setName("用户线程");
         t.start();
         tuser.start();
     }  
     public void run() {  
-        boolean isDaemon = Thread.currentThread().isDaemon();  
+        boolean isDaemon = Thread.currentThread().isDaemon();
+        String name = Thread.currentThread().getName();
         try {  
-            StringBuffer sb = new StringBuffer();  
-            sb.append("start Thread, daemon:" + isDaemon);  
+            StringBuilder sb = new StringBuilder();
+            sb.append(name);
+            sb.append(" start Thread, daemon:");
+            sb.append(isDaemon);
             if (isDaemon) {  
                 sb.append(", 'finally does not run!'");
                 Thread.sleep(1000L);
@@ -29,7 +34,7 @@ public class DaemonAndFinally implements Runnable {
                 System.out.println("Exiting via InterruptedException");  
             }  
         } finally {  
-            System.out.println("Finally has run.");  
+            System.out.println(name  +" Finally has run.");
         }  
     }  
 } 
