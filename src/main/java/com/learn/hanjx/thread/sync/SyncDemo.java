fe8ed1ec997d523ee8db3130b06621322ae85b9f
hanjx <511572653@qq.com>
2018-03-01 17:59:29 +0800

    同步学习

--- src/main/java/com/learn/hanjx/thread/sync/SyncDemo.java
+++ src/main/java/com/learn/hanjx/thread/sync/SyncDemo.java
@@ -1,24 +1,121 @@
 package com.learn.hanjx.thread.sync;
 
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class SyncDemo implements Runnable{
+/**
+ * 线程同步
+ * synchronized 关键字
+ * volatile
+ */
+public class SyncDemo implements Runnable
+{
 
-	@Override
-	public void run() {
-		synchronized(this.getClass()){
-			for (int i = 0; i < 5; i++) {
-				System.out.println(Thread.currentThread());
-			}			
-		}
+    @Override
+    public void run()
+    {
+        synchronized (this.getClass())
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.out.println(Thread.currentThread());
+            }
+        }
 
-		
-	}
-	public static void main(String[] args) {
-		SyncDemo sd = new SyncDemo();
-		Thread t1 = new Thread(sd,"thread1");
-		Thread t2 = new Thread(sd,"thread2");
-		t1.start();
-		t2.start();
-	}
+
+    }
+
+    public static void main(String[] args)
+    {
+        SyncDemo sd = new SyncDemo();
+        Thread t1 = new Thread(sd, "thread1");
+        Thread t2 = new Thread(sd, "thread2");
+        t1.start();
+        t2.start();
+    }
+    public static void mainDemo(String[] args)
+    {
+        SampleSynchronized sampleSynchronized = new SampleSynchronized();
+        Thread synchronized1 = new Thread(sampleSynchronized, "thread1");
+        Thread synchronized2 = new Thread(sampleSynchronized, "thread2");
+        synchronized1.start();
+        synchronized2.start();
+    }
 
 }
+
+/**
+ * synchronized 关键字
+ */
+class SampleSynchronized implements Runnable
+{
+    private static int count = 0;
+
+    @Override
+    public synchronized void run()
+    {
+        count++;
+    }
+}
+/**
+ * lock 锁  重量级
+ */
+class SampleLock implements Runnable
+{
+    private static int count = 0;
+    private Lock lock = new ReentrantLock();
+    @Override
+    public synchronized void run()
+    {
+        lock.lock();
+        count++;
+        lock.unlock();
+    }
+}
+/**
+ * 原子、原子更新类
+ * {@link java.util.concurrent.atomic}
+ */
+class SampleAtomicInteger implements Runnable
+{
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    @Override
+    public synchronized void run()
+    {
+        count.getAndIncrement();
+    }
+}
+
+/**
+ * volatile 关键字
+ * 语义：易变的，不稳定的
+ * 加锁机制既可以确保可见性又可以确保原子性，而volatile变量确保多线程时的可见性。
+ */
+class SampleVolatile implements Runnable
+{
+    private static volatile int count = 0;
+    @Override
+    public synchronized void run()
+    {
+        count++;
+    }
+}
+/* 普通加锁：以时间换空间
+ * ThreadLocalVariable 线程局部变量
+ * 生成很多变量副本，不同线程同时操作，以空间换时间
+ */
+class SampleThreadLocal implements Runnable
+{
+    ThreadLocal<Integer> count = new ThreadLocal<>();
+    @Override
+    public synchronized void run()
+    {
+        int count1=count.get();
+        count1++;
+    }
+}
+
+
